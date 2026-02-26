a7c9c9734945415b043601ff8fd5e53fd1ff7d5a
Clement Ghirardi <cghirard@f6r9s1.clusters.42paris.fr>
2026-02-26 15:06:34 +0100

    worked on expander

--- srcs/expander.c
+++ srcs/expander.c
@@ -6,22 +6,43 @@
 /*   By: cghirard <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/25 13:41:43 by cghirard          #+#    #+#             */
-/*   Updated: 2026/02/25 23:41:11 by cghirard         ###   ########.fr       */
+/*   Updated: 2026/02/26 14:13:02 by cghirard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-char	*extract_var_name(char *str, int *i);
-char	*join_and_free(char *s1, char *s2);
+int	is_var_char(char c)
+{
+	char	*valid_chars;
+
+	if (('a' <= c && c <= 'z')
+		|| ('A' <= c && c <= 'Z')
+		|| ('0' <= c && c <= '9')
+		|| (c == '_'))
+		return (1);
+	return (0);
+}
+
+char	*extract_var_name(char *str, int *i)
+{
+	int	start;
+
+	start = *i;
+	while (is_var_char(str[*i]))
+		(*i)++;
+	return (ft_substr(str, start, *i - start));
+}
 
 char	*expand_string(char *str, t_quote quote, char **envp)
 {
 	int		i;
 	char	*result;
 	char	*var;
-	char	tmp[2];
+	char	*tmp;
 
+	if (quote == SQUOTE)
+		return (str);
 	i = 0;
 	*result = ft_strdup("");
 	while (str[i])
@@ -30,16 +51,19 @@ char	*expand_string(char *str, t_quote quote, char **envp)
 		{
 			i++;
 			var = extract_var_name(str, &i);
-			result = join_and_free(result, getenv(var));
-			free(var);
+			tmp = ft_strjoin(result, getenv(var));
 		}
 		else
 		{
-			tmp[0] = str[i];
-			tmp[1] = '\0';
-			result = join_and_free(result, tmp);
+			var = malloc (2 * sizeof(char));
+			var[0] = result[i];
+			var[1] = '\0';
+			tmp = ft_strjoin(result, var);
 			i++;
 		}
+		free(result);
+		free(var);
+		result = tmp;
 	}
 	return (result);
 }
