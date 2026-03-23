6ab5d7bdb4d6e4def6fb85e655062dc4e03e681d
Adele Barth <adbarth@f6r4s4.clusters.42paris.fr>
2026-03-23 14:54:26 +0100

    lexer update

--- srcs/lexer.c
+++ srcs/lexer.c
@@ -12,8 +12,14 @@
 
 #include "../includes/minishell.h"
 
-static void	handle_pipe(t_token **tokens, int *i)
+static void	handle_pipe(char *input, t_token **tokens, int *i)
 {
+	if (input[*i + 1] == '|')
+	{
+		add_token(tokens, new_token(TOKEN_OR, "||"));
+		*i += 2;
+		return ;
+	}
 	add_token(tokens, new_token(TOKEN_PIPE, "|"));
 	(*i)++;
 }
@@ -64,6 +70,17 @@ static void	handle_quotes(char *input, t_token **tokens, int *i)
 		(*i)++;
 }
 
+static void	handle_and(char *input, t_token **tokens, int *i)
+{
+	if (input[*i + 1] == '&')
+	{
+		add_token(tokens, new_token(TOKEN_AND, "&&"));
+		*i += 2;
+		return ;
+	}
+	(*i)++;
+}
+
 static void	handle_word(char *input, t_token **tokens, int *i)
 {
 	int		start;
@@ -95,15 +112,117 @@ t_token	*lexer(char *input)
 		if (input[i] == ' ')
 			i++;
 		else if (input[i] == '|')
-			handle_pipe(&tokens, &i);
+			handle_pipe(input, &tokens, &i);
 		else if (input[i] == '>')
 			handle_redir(input, &tokens, &i, 1);
 		else if (input[i] == '<')
 			handle_redir(input, &tokens, &i, 0);
 		else if (input[i] == '\'' || input[i] == '"')
 			handle_quotes(input, &tokens, &i);
+		else if (input[i] == '&')
+			handle_and(input, &tokens, &i);
 		else
 			handle_word(input, &tokens, &i);
 	}
 	return (tokens);
 }
+
+
+
+// #include "../includes/minishell.h"
+
+// static void	handle_pipe(t_token **tokens, int *i)
+// {
+// 	add_token(tokens, new_token(TOKEN_PIPE, "|"));
+// 	(*i)++;
+// }
+
+// static void	handle_redir(char *input, t_token **tokens, int *i, int dir)
+// {
+// 	if (dir == 1)
+// 	{
+// 		if (input[*i + 1] == '>')
+// 		{
+// 			add_token(tokens, new_token(TOKEN_APPEND, ">>"));
+// 			*i += 2;
+// 		}
+// 		else
+// 		{
+// 			add_token(tokens, new_token(TOKEN_REDIR_OUT, ">"));
+// 			(*i)++;
+// 		}
+// 		return ;
+// 	}
+// 	if (input[*i + 1] == '<')
+// 	{
+// 		add_token(tokens, new_token(TOKEN_APPEND, "<<"));
+// 		*i += 2;
+// 	}
+// 	else
+// 	{
+// 		add_token(tokens, new_token(TOKEN_REDIR_OUT, "<"));
+// 		(*i)++;
+// 	}
+// }
+
+// static void	handle_quotes(char *input, t_token **tokens, int *i)
+// {
+// 	char	quote;
+// 	int		start;
+// 	char	*word;
+
+// 	quote = input[*i];
+// 	(*i)++;
+// 	start = *i;
+// 	while (input[*i] && input[*i] != quote)
+// 		(*i)++;
+// 	word = ft_substr(input, start, *i - start);
+// 	add_token(tokens, new_token(TOKEN_WORD, word));
+// 	free(word);
+// 	if (input[*i] == quote)
+// 		(*i)++;
+// }
+
+// static void	handle_word(char *input, t_token **tokens, int *i)
+// {
+// 	int		start;
+// 	char	*word;
+
+// 	start = *i;
+// 	while (input[*i]
+// 		&& input[*i] != ' '
+// 		&& input[*i] != '|'
+// 		&& input[*i] != '<'
+// 		&& input[*i] != '>'
+// 		&& input[*i] != '\''
+// 		&& input[*i] != '"')
+// 		(*i)++;
+// 	word = ft_substr(input, start, *i - start);
+// 	add_token(tokens, new_token(TOKEN_WORD, word));
+// 	free(word);
+// }
+
+// t_token	*lexer(char *input)
+// {
+// 	t_token	*tokens;
+// 	int		i;
+
+// 	tokens = NULL;
+// 	i = 0;
+// 	while (input[i])
+// 	{
+// 		if (input[i] == ' ')
+// 			i++;
+// 		else if (input[i] == '|')
+// 			handle_pipe(&tokens, &i);
+// 		else if (input[i] == '>')
+// 			handle_redir(input, &tokens, &i, 1);
+// 		else if (input[i] == '<')
+// 			handle_redir(input, &tokens, &i, 0);
+// 		else if (input[i] == '\'' || input[i] == '"')
+// 			handle_quotes(input, &tokens, &i);
+// 		else
+// 			handle_word(input, &tokens, &i);
+// 	}
+// 	return (tokens);
+// }
