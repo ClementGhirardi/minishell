5fd1aff5a7c347c7a2991ebb8da0aac104ae348b
Adele Barth <adbarth@f2r7s2.clusters.42paris.fr>
2026-04-28 14:51:58 +0200

    ft_exit improved

--- srcs/ft_exit.c
+++ srcs/ft_exit.c
@@ -17,16 +17,21 @@ int	ft_exit(t_ast **ast, char **args, char ***env)
 	ft_printf("exit\n");
 	if (args && args[0] && args[1])
 	{
-		if (!args[2])
-			status = ft_atoi(args[1]);
-		else
+		if (!ft_isdigit(args[1][0]))
 		{
-			ft_putendl_fd("minishell: exit: too many arguments\n", 2);
+			ft_putstr_fd("minishell: exit: ", 2);
+			ft_putstr_fd(args[1], 2);
+			ft_putendl_fd(": numeric argument required", 2);
+			status = 2;
+		}
+		else if (args[2])
+		{
+			ft_putendl_fd("minishell: exit: too many arguments", 2);
 			status = 1;
-			if (ast)
-				ast_free(*ast);
 			return (status);
 		}
+		else
+			status = ft_atoi(args[1]);
 	}
 	if (ast)
 		ast_free(*ast);
